cfd1504ff5253dbebe15a60608bf0764b52a0f32
Akpughe <davidakpughe2@gmail.com>
2023-10-19 22:55:27 +0100

    interfaces

---
+++ src/types/index.ts
@@ -0,0 +1,27 @@
+export interface ICar {
+  id: string;
+  title: string;
+  year: string;
+  imageUrl: string;
+  marketplacePrice: number;
+  marketplaceOldPrice: number;
+  mileage: number;
+  transmission: string;
+  sellingCondition: string;
+  engineType: string;
+  installment: number;
+  carName: string;
+  gradeScore: number;
+  state: string;
+  country: string;
+  city: string;
+  interiorColor: string;
+  exteriorColor: string;
+  fuelType: string;
+}
+
+export interface ICarMedia {
+  id: string;
+  name: string;
+  url: string;
+}
