ba733de1777d2df99cbe9d47e4753a4e48208afa
Akpughe <davidakpughe2@gmail.com>
2023-10-19 21:56:24 +0100

    [add] css

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import "@mantine/core/styles.css";
 import "@mantine/carousel/styles.css";
 import { Header } from "@/components";
 import Providers from "@/utils";
