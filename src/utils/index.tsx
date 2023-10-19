2f9523a82cc681709734082fef2520a32f21c124
Akpughe <davidakpughe2@gmail.com>
2023-10-19 17:22:00 +0100

    add react query

--- src/utils/index.tsx
+++ src/utils/index.tsx
@@ -2,9 +2,18 @@
 
 import React from "react";
 import { MantineProvider, createTheme } from "@mantine/core";
+import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 function Providers({ children }: React.PropsWithChildren) {
-  return <MantineProvider>{children}</MantineProvider>;
+  const [client] = React.useState(new QueryClient());
+
+  return (
+    <QueryClientProvider client={client}>
+      <MantineProvider>{children}</MantineProvider>
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
+  );
 }
 
 export default Providers;
