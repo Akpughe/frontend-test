848dec97cae58b577aa530a89a472de4d286b20a
Akpughe <davidakpughe2@gmail.com>
2023-10-19 22:55:48 +0100

    apis

--- src/service/api.ts
+++ src/service/api.ts
@@ -1,6 +1,7 @@
 import axios from "axios";
 
 const API_URL = `https://api.staging.myautochek.com/v1`;
+const API_URL_PROD = `https://api-prod.autochek.africa/v1`;
 
 export const getPopularBrands = async () => {
   const response = await axios.get(`${API_URL}/inventory/make?popular=true`);
@@ -9,19 +10,21 @@ export const getPopularBrands = async () => {
 };
 
 export const getAllCars = async () => {
-  const response = await axios.get(`${API_URL}/inventory/car/search`);
+  const response = await axios.get(`${API_URL_PROD}/inventory/car/search`);
 
   return response.data;
 };
 
 export const getCarDetails = async (carId: string) => {
-  const response = await axios.get(`${API_URL}/inventory/car/${carId}`);
+  const response = await axios.get(`${API_URL_PROD}/inventory/car/${carId}`);
 
   return response.data;
 };
 
 export const getCarMedia = async (carId: string) => {
-  const response = await axios.get(`${API_URL}/inventory/car/${carId}`);
+  const response = await axios.get(
+    `${API_URL_PROD}/inventory/car_media?carId=${carId}`
+  );
 
   return response.data;
 };
